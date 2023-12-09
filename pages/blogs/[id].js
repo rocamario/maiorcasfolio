1952d293e637ca6fd16305647ee68266ef99d560
Mario Roca <marioroca@MacBook-Pro-di-Mario.local>
2023-12-09 20:05:05 +0100

    fixes

--- pages/blogs/[id].js
+++ pages/blogs/[id].js
@@ -4,7 +4,6 @@ import Navbar from "../../Components/Navbar";
 import Footer from "../../Components/Footer";
 import Head from "next/head";
 import BlogInner from "../../Components/BlogInner";
-import BlogShare from "../../Components/BlogShare";
 import Comments from "../../Components/Comments";
 import { SWRConfig } from "swr";
 import { remarkHeadingId } from "remark-custom-heading-id";
